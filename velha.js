7ba42d8e3680ff98a36a6102164f513776315922
Allysonubius <Allysomoliveira7@gmail.com>
2020-09-06 19:52:38 -0300

    Adicionado escolha do jogador X.

--- velha.js
+++ velha.js
@@ -1,14 +1,19 @@
 var jogador = null;
+
 var jogadorSelecionado = document.getElementById('jogador-selecionado');
 
+mudarJogador('X');
+
 function escolherQuadrado(id) {
+
     var quadrado = document.getElementById(id);
 
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#000';
+
 }
 
 function mudarJogador(valor) {
     jogador = valor;
-    jogador
+    jogadorSelecionado.innerHTML = jogador;
 }
